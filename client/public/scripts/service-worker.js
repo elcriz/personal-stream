83d7ca898cab237614ddcca6e5bd3380d8b8973a
GAME-PC-CHRISTI\Christiaan Hemerik <elcriz@gmail.com>
2024-12-08 11:59:04 +0100

    adds debug logging

--- client/public/scripts/service-worker.js
+++ client/public/scripts/service-worker.js
@@ -9,6 +9,8 @@ self.addEventListener('activate', (event) => {
 self.addEventListener('push', (event) => {
   const { title, body } = event.data.json();
 
+  console.debug('Push received', { title, body });
+
   event.waitUntil(
     self.registration.showNotification(title, { body }),
   );
